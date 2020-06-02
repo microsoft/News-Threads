3857f6a501483d94c49e2869b5a900289adf6232
Jonathan McLean <jonmclean@users.noreply.github.com>
2020-06-02 15:41:08 -0700

    Added check for output paths and input data before starting import

--- newsthreads/__init__.py
+++ newsthreads/__init__.py
@@ -5,7 +5,8 @@ from .combinedClusterLabels import combine_cluster_labels
 from .sentences import process_sentences
 from .domain_dependency_graph import domain_dependency_graph
 from .build_hierarchy import build_hierarchy
+from .paths import ensure_paths_exist
 
 __all__ = ["get_common_config", "get_dataset_config", "get_epsilon_cluster_filename", "get_logger", "build_minhash",
            "calculate_jaccard", "perform_dbscan", "combine_cluster_labels", "process_sentences",
-           "domain_dependency_graph", "build_hierarchy"]
+           "domain_dependency_graph", "build_hierarchy", "ensure_paths_exist"]
