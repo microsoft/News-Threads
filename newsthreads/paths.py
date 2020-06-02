adb95da2d7f0233a14b676427b23ddd59a2b0da4
Jon McLean <none@example.com>
2020-06-02 15:46:52 -0700

    Lint fixes

--- newsthreads/paths.py
+++ newsthreads/paths.py
@@ -5,6 +5,7 @@ from . import get_logger
 
 logger = get_logger(os.path.basename(__file__))
 
+
 def ensure_paths_exist(config: configparser.SectionProxy):
     """Ensures that input data and output paths exist"""
     output_path = config['OutputPath']
@@ -16,7 +17,7 @@ def ensure_paths_exist(config: configparser.SectionProxy):
     if not os.path.exists(output_path):
         logger.info(f"Creating path {output_path}")
         os.makedirs(output_path)
-    
+
     if not os.path.exists(input_file):
         logger.error(f"Input file {input_file} does not exist")
-        sys.exit(1) # Force interpreter to exit after error message above
+        sys.exit(1)  # Force interpreter to exit after error message above
